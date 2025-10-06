923ab27fdb57a58a2444cb0bf4e1ebbd9a425cf2
Malick Ouedraogo <C00400598@louisiana.edu>
2025-10-06 11:20:07 -0500

    Setup global auth state and protected routes

---
+++ src/Auth/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useAuth } from "./AuthContext";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+  if (!user) {
+    return <Navigate to={"/"} replace/>;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
