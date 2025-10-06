075c974f7662e7576fd58c9025f59101773a1303
Malick Ouedraogo <C00400598@louisiana.edu>
2025-10-06 06:41:43 -0500

    Established landing page with login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { useState } from "react";
+import LandingPage from "./pages/Landing/LandingPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  
+
   return (
     <>
-      <Navbar />
+    {
+      isLoggedIn && <Navbar />
+    }
       <Routes>
-        <Route path="/" element={<h1>Login Page</h1>} />
+        <Route path="/" element={<LandingPage/>} />
         <Route path="/profile" element={<h1>Profile Page</h1>} />
         <Route path="/explore" element={<h1>Explore Page</h1>} />
         <Route path="/create" element={<h1>Create Page</h1>} />
